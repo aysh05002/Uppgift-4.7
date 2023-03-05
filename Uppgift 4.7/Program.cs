a5dd5e1d5ab934517695575052b1291aada001b5
Souzan <Souzan@DESKTOP-VHKB7GI>
2023-03-05 15:47:34 +0200

    Add project files.

---
+++ Uppgift 4.7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in ett meddelande");
+        string meddelande = Console.ReadLine();
+        Console.WriteLine("Skriv ett tal för hur många steg ska medelandet för flytas åt höger");
+        int steg = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < meddelande.Length; i++)
+        {
+            for (int j = 0; j <= steg; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine(meddelande[i]);
+        }
+    }
+}
